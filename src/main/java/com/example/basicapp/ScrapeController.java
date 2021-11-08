7d68fb5af58f734320bb2a463edc472c00d2a61a
Miguel Andrade <miguel.andrade@vilt.local>
2021-11-08 15:08:53 +0000

    [DOCS] commented code

--- src/main/java/com/example/basicapp/ScrapeController.java
+++ src/main/java/com/example/basicapp/ScrapeController.java
@@ -11,22 +11,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-// import org.jsoup.nodes.Element;
-// import org.jsoup.select.Elements;
 
 @Controller
 public class ScrapeController {
     
+    // HashMap responsible for storing the data of the url and the fetched title
     public HashMap<String, String> listGlobal = new HashMap<>();
 
+    // route for the scrape page
     @GetMapping("/scrape")
     public String scrape(Model model) {
-        String result = "";
-        String url = "https://www.rtp.pt/";
+        String result = ""; // variable that will store the fetched title
+        String defaultUrl = "https://www.rtp.pt/"; // default url
 
+        // Starts the fecth for the given url, in this page there is a default url
         try {
             // Here we create a document object and use JSoup to fetch the website
-            Document doc = Jsoup.connect(url).get();
+            Document doc = Jsoup.connect(defaultUrl).get();
      
             // With the document fetched, we use JSoup's title() method to fetch the title
             result = doc.title().toString();
@@ -36,24 +37,27 @@ public class ScrapeController {
             e.printStackTrace();
           }
           
-
-        model.addAttribute("url", "URL: " + url);
-        model.addAttribute("title", "Título" + result);
         
-        return "scrape";
+        model.addAttribute("url", "URL: " + defaultUrl); // binds the url to the variable url in the html file
+        model.addAttribute("title", "Título" + result); // binds the result to the variable title in the html file
+        
+        return "scrape"; // return the scrape html file as the view
     }
 
+    // Post request reponsible to fetch the title from the give url and stores the data in the global variable listGlobal
     @PostMapping(value = "/scrapesite", params = "submit")
     public String scrapeSubmit(@ModelAttribute("url") String url, Model model) {
 
-      String result = "";
-      String finalList = "";
+      String result = ""; // variable that will store the fetched title
+      String finalList = ""; // variable that concats all the info showed in the list
 
+      // if the url is empty returns to the scrape page
       if(url.isEmpty())
       {
         return "scrape";
       }
 
+      // Starts the fetch for the given url, in this page there is a default url
       try {
           // Here we create a document object and use JSoup to fetch the website
           Document doc = Jsoup.connect(url).get();
@@ -66,44 +70,49 @@ public class ScrapeController {
           e.printStackTrace();
         }
 
+        // Checks if the url is already stored, if not adds it
         if(!listGlobal.containsKey(url))
         {
           listGlobal.put(url, result);
         }
 
+        // Loops the listGlobal and constructs a string with all the html tags to export for the view page 
         Object[] urls = listGlobal.keySet().toArray();
 
         for(int i = 0; i < urls.length; i++) {
           finalList += "<form th:action=\"@{/scrapesite}\" method=\"post\"><li><p> SITE: " + urls[i].toString() +"</p><p> TÍTULO: " + listGlobal.get(urls[i].toString()) + "</p><input type=\"hidden\" name=\"url\" value=" + urls[i].toString() +"/><input name=\"cancel\" type=\"submit\" value=\"Remover\"/></li></form>";
         }
 
-        model.addAttribute("title", "");
-        model.addAttribute("url", "");
-        model.addAttribute("list", finalList);
+        model.addAttribute("title", ""); // binds the url to the variable url in the html file, in this case I want an empty string 
+        model.addAttribute("url", ""); // binds the result to the variable title in the html file, in this case I want an empty string 
+        model.addAttribute("list", finalList); // binds the final string to the variable list in the html file 
 
-      return "scrape";
+      return "scrape"; // return the scrape html file as the view
     }
 
+    // Deletes the given url from the listGlobal variable
     @PostMapping(value = "/scrapesite", params = "cancel")
     public String deleteItem(@ModelAttribute("url") String url, Model model) {
 
-      String finalList = "";
+      String finalList = ""; // variable that concats all the info showed in the list
 
-      //Sending the url via value param from the input tag, adds a symbol and needs to be removed before the operation
+      // Sending the url via value param from the input tag adds a symbol and it needs to be removed before the operation
       String urlTrimmed = url.toString().replaceAll(".$", "");
-
       listGlobal.remove(urlTrimmed);
 
+      
+      // Loops the listGlobal and constructs a string with all the html tags to export for the view page 
       Object[] urls = listGlobal.keySet().toArray();
 
       for(int i = 0; i < urls.length; i++) {
         finalList += "<form th:action=\"@{/scrapesite}\" method=\"post\"><li><p> SITE: " + urls[i].toString() +"</p><p> TÍTULO: " + listGlobal.get(urls[i].toString()) + "</p><input type=\"hidden\" name=\"url\" value=" + urls[i] +"/><input name=\"cancel\" type=\"submit\" value=\"Remover\"/></li></form>";
       }
 
-      model.addAttribute("url", "");
-      model.addAttribute("title", "");
-      model.addAttribute("list", finalList);
-      return "scrape";
+      model.addAttribute("title", ""); // binds the url to the variable url in the html file, in this case I want an empty string 
+      model.addAttribute("url", ""); // binds the result to the variable title in the html file, in this case I want an empty string 
+      model.addAttribute("list", finalList); // binds the final string to the variable list in the html file 
+
+      return "scrape"; // return the scrape html file as the view
     }
 
 }
